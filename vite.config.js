67eca2bcbd34f97874bcd96979598046966e08ff
Jack <958691165@qq.com>
2023-08-22 18:20:31 +0800

    transform-vitejs

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ const path = require('path')
 // https://vitejs.dev/config/
 
 export default defineConfig({
-
+    base: './',
     plugins: [vue()],
     resolve: {
         extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
